deca1264920dd4a4f403506ccc8287560a09dbfb
dokoliday <dokoliday@hotmail.fr>
2019-10-22 14:52:08 +0200

    add : body parser && routes channels et messages

--- api/server.js
+++ api/server.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const bodyParser = require('body-parser');
+
+const routerChannel = require('./routes/routerChannel');
+const routerMessage = require('./routes/routerMessage');
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use('/api/channels', routerChannel);
+app.use('/api/messages', routerMessage);
+
+
 
 app.listen(4000, function () {
-  console.log('Example app listening on port 4000!')
+    console.log('Example app listening on port 4000!')
 })
