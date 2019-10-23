013c6f9a6161cd4d5a15461f347a36bfb0ca51d2
dokoliday <dokoliday@hotmail.fr>
2019-10-23 16:01:57 +0200

    add queries update channel and message

---
+++ api/dataHandler/queries/updateChannel.js
@@ -0,0 +1,14 @@
+const { connect } = require("../connect");
+
+const updateChannel = async (name, channelId) => {
+    try {
+        const data = await connect
+            .query(`UPDATE channel SET name=($1) WHERE id=($2)`, [name, channelId]);
+        return data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw (error);
+    }
+};
+
+module.exports = { updateChannel };
---
+++ api/dataHandler/queries/updateMessage.js
@@ -0,0 +1,14 @@
+const { connect } = require("../connect");
+
+const updateMessage= async (name, messageId) => {
+    try {
+        const data = await connect
+            .query(`UPDATE message SET content=($1) WHERE id=($2)`, [name, messageId]);
+        return data;
+    } catch (error) {
+        console.log('error: ', error);
+        throw (error);
+    }
+};
+
+module.exports = { updateMessage };
