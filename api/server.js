ec5c099e911f3d3c6707ecb4f272bb7a375f1de3
dokoliday <dokoliday@hotmail.fr>
2019-10-23 13:04:54 +0200

    mmise en place de gaphql

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const app = express();
 
 const bodyParser = require('body-parser');
 
 const routerChannel = require('./routes/routerChannel');
 const routerMessage = require('./routes/routerMessage');
+
+const { typeDefs, resolvers }  = require('./graphql');
 
 app.use(bodyParser.json());
 app.use(
@@ -15,8 +18,9 @@ app.use(
 app.use('/api/channels', routerChannel);
 app.use('/api/messages', routerMessage);
 
-
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
 
 app.listen(4000, function () {
     console.log('Example app listening on port 4000!')
-})
+});
