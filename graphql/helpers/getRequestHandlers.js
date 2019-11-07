457e7ea4f31bcd89b430a9370446f939788228ad
dokoliday <dokoliday@hotmail.fr>
2019-11-07 10:45:13 +0100

    refacto

--- graphql/helpers/getRequestHandlers.js
+++ graphql/helpers/getRequestHandlers.js
@@ -3,7 +3,6 @@ const fs = require("fs")
 const getRequestHandlers = (directory) => {
    return fs.readdirSync(directory).reduce((acc, file) => {
       if (file !== "index.js" && /[a-zA-Z0-9\-]*.js$/.test(file)) {
-         // console.log(`charge du fichier ${file}`)
          const requiredFile = require(`${directory}/${file}`);
          Object.assign(acc, requiredFile);
       }
