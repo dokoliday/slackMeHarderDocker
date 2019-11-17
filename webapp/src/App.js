f1c8677fa5bc424434a93b82dcfccca6f61a5380
dokoliday <dokoliday@hotmail.fr>
2019-11-17 19:08:14 +0100

    REFACTO

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import apolloClient from './config/apolloSetup';
 import { ApolloProvider } from '@apollo/react-hooks';
-import Home from './component/Channels'
-
+import Home from './component/Home'
 const App = () => (
   <ApolloProvider client={apolloClient}>
     <Home/>
   </ApolloProvider>
+
 );
 
 export default App;
