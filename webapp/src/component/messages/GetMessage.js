78ee1240d5d184ec125eea0331a2a5719b392a6f
dokoliday <dokoliday@hotmail.fr>
2019-11-25 15:08:11 +0100

    MOD : front when no message

--- webapp/src/component/messages/GetMessage.js
+++ webapp/src/component/messages/GetMessage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react'
 import MessageUpdateForm from './MessageUpdateForm';
 import MessageDeletedButton from './MessageDeleteButton';
 import styled from 'styled-components';
@@ -8,16 +8,34 @@ const WrapperMessages = styled.div`
 flex:4
 background-color:#8E99E6;
 `;
+const CenteredParagraph = styled.p`
+margin-top: 50vh;
+text-align:center;
+`
+
+
 const GetMessages = (props) => {
-   if(props.messagesBychannel===0){
-      return "no messages"
-   }return (
+
+   const messagesEndRef = useRef(null)
+
+   const scrollToBottom = () => {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+   }
+
+   // useEffect(scrollToBottom, [props.data.messagesBychannel]);
+
+   if (props.data.messagesBychannel.length === 0) {
+      return <CenteredParagraph>This is your first message</CenteredParagraph>
+
+   }
+   return (
       <WrapperMessages>
          {props.data.messagesBychannel.map((message, index) =>
-            <div key={index}>
+            <div key={index} ref={messagesEndRef} >
                <p>{message.content}</p>
                <MessageUpdateForm id={message.id} />
-               <MessageDeletedButton id={message.id} channelId={props.channelId}/>
+               <MessageDeletedButton id={message.id} channelId={props.channelId} />
+               <div ref={messagesEndRef} />
             </div>
          )}
       </WrapperMessages>
